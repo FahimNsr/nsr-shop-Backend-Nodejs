a3f390e2f7b2c059fed314085f9da288438fcb28
FahimNsr <fahim.nasirzadeh@gmail.com>
2021-10-02 12:11:33 +0200

    fix bugs verifyAdmin

--- middlewares/verifyAdmin.js
+++ middlewares/verifyAdmin.js
@@ -1,19 +1,20 @@
 const jwt = require("jsonwebtoken");
 
 const verifyAdmin = (req, res, next) => {
-    
+    const token =
+        req.body.token ||
+        req.query.token ||
+        req.headers["x-access-token"] ||
+        (req.get("Authorization") ? req.get("Authorization").split(" ")[1] : null)
+    if (!token) {
+        return res.status(403).send("A token is required for authentication");
+    }
     try {
-        const token = req.body.token || req.query.token || req.headers["x-access-token"] || req.get("Authorization").split(" ")[1];
-        if (!token) {
-            return res.status(403).send("A token is required for authentication");
-        }
-
         const decodedToken = jwt.verify(token, process.env.JWT_KEY);
         if (!decodedToken && !decodedToken.admin) {
             return res.status(403).send("You do not have permission");
         }
         req.user = decodedToken;
-
     } catch (err) {
         return res.status(401).send("Invalid Token");
     }
